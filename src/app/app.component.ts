12c83a486c0f923d93e650da5997980f6d02c1e3
José Queiroz <jqueiroz.av@gmail.com>
2025-09-29 09:34:38 -0300

    Legacy angular (ngFor and ngIf)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+import { NgFor, NgIf } from '@angular/common';
 import { DUMMY_USERS } from './dummy-users';
+import { HeaderComponent } from './header/header.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
