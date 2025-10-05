1dfecf3edce4425ae8eb3cbce80fdf3a89af29c7
José Queiroz <jqueiroz.av@gmail.com>
2025-10-05 20:02:34 -0300

    Refactored main app to be module based

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 
 import { DUMMY_USERS } from './dummy-users';
-import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -17,7 +13,7 @@ export class AppComponent {
   selectedUserId?: string;
 
   get selectedUser() {
-    return this.users.find(user => user.id === this.selectedUserId);
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
   onSelectUser(id: string) {
