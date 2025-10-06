99444c0d75a45256e877ce7d002658074c161c24
José Queiroz <jqueiroz.av@gmail.com>
2025-10-06 19:53:45 -0300

    Migrating all components to use modules

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-@Component( {
-    selector: 'app-header',
-    standalone: true, //(default on Angular 19+)
-    // template: '<h1>EasyTask</h1>',
-    templateUrl: './header.component.html',
-    // styles: ['h1 { color: red }'],
-    styleUrl: './header.component.css'
+@Component({
+  selector: 'app-header',
+  standalone: false, //(default on Angular 19+)
+  // template: '<h1>EasyTask</h1>',
+  templateUrl: './header.component.html',
+  // styles: ['h1 { color: red }'],
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {}
